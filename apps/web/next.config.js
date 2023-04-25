9d4b0396e47ffcfa0baac837bef5dca72d70ed1f
GashonHussein <gashon96@gmail.com>
2023-04-24 23:01:43 -0700

    chore: setup bundle analyzer

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,29 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+  openAnalyzer: true,
+});
+
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
-  transpilePackages: ["ui"],
-};
+  experimental: {
+    transpilePackages: ['@scrib/config/tsconfig', '@scrib/ui'],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  poweredByHeader: false,
+  eslint: { ignoreDuringBuilds: true },
+  async rewrites() {
+    const HOST = 'http://localhost:7000'; //env
+    return [
+      {
+        source: '/ajax/:path*',
+        destination: `${HOST}/ajax/:path*`,
+      },
+      {
+        source: '/api/:path*',
+        destination: `${HOST}/api/:path*`,
+      },
+    ];
+  },
+});
