edd9993a01828f54f7d708edf455f1a13bc491b6
GashonHussein <gashon96@gmail.com>
2023-04-26 17:08:12 -0700

    refactor: add docker to scripts

--- apps/api/src/graphql/schema/root/mutation.ts
+++ apps/api/src/graphql/schema/root/mutation.ts
@@ -1,6 +1,15 @@
-import { GraphQLObjectType } from 'graphql';
+import { GraphQLID, GraphQLNonNull, GraphQLObjectType } from 'graphql';
+
+//tmp
 
 export const mutation = new GraphQLObjectType({
   name: 'Mutation',
-  fields: (): any => ({}),
+  fields: (): any => ({
+    // todo
+    // tmp field for compilation
+    tmp: {
+      type: new GraphQLNonNull(GraphQLID),
+      description: 'Globally unique ID of the post',
+    },
+  }),
 });
