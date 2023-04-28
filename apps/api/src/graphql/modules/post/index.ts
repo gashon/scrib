62458488107b15be3b92122f33cba601e1bb85d4
GashonHussein <gashon96@gmail.com>
2023-04-27 22:23:38 -0700

    fix: rmv modules

--- apps/api/src/graphql/modules/post/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./post.schema"
-export * from "./post.resolvers"
--- apps/api/src/graphql/schema/types/edges/post-edge.ts
+++ apps/api/src/graphql/schema/types/edges/post-edge.ts
@@ -5,10 +5,10 @@ export const postEdge = new GraphQLObjectType({
   name: 'PostEdge',
   description: 'List of edges.',
   fields: {
-    // node: {
-    //   description: 'The item at the end of the edge.',
-    //   type: postType,
-    // },
+    node: {
+      description: 'The item at the end of the edge.',
+      type: postType,
+    },
     cursor: {
       description: 'A cursor for pagination.',
       type: GraphQLString,
