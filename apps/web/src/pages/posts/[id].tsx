f4f7e21cbe756ef189bde1bdfa2ff89358105333
GashonHussein <gashon96@gmail.com>
2023-05-11 23:12:14 -0700

    feat: posts/:id page

--- apps/web/src/pages/posts/[id].tsx
+++ apps/web/src/pages/posts/[id].tsx
@@ -1,26 +1,37 @@
-import { FC } from 'react';
-import { useLazyLoadQuery } from 'react-relay';
-import { GET_POST_QUERY } from '@scrib/web/features/author';
-import { PostContainer } from '@scrib/web/features/post';
+import React from 'react';
+import Editor from '@scrib/editor/src';
+import { useMutation } from 'react-relay';
+import { UPDATE_POST } from '@scrib/web/features/post';
+import { getPost } from '@scrib/web/features/post';
+import { IPost } from '@scrib/db/models/post';
 
-const PostPage: FC<{ postId: string }> = ({ postId }) => {
-  const query = useLazyLoadQuery(GET_POST_QUERY, {
-    id: postId,
-  });
-
-  return (
-    <>
-      <PostContainer query={query.post} />
-    </>
-  );
+type Props = {
+  post: Partial<IPost>;
 };
 
-export async function getServerSideProps({ params }) {
+export default function Post({ post }: Props) {
+  return (
+    <div className="w-screen h-min-screen flex p-10 justify-center">
+      <div className="w-full h-full flex flex-col w-3/4">
+        <h1 className="text-3xl mb-10 underline font-bold">
+          {post.title ?? 'Post'}
+        </h1>
+        <div className="">
+          <Editor readOnly={true} defaultValue={post.content} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const { id } = context.params;
+
+  const post = await getPost(id);
+
   return {
     props: {
-      postId: params.id,
+      post,
     },
   };
 }
-
-export default PostPage;
