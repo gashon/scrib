c918891058fb7ae65461d31aeed0534366779a95
GashonHussein <gashon96@gmail.com>
2023-05-12 02:22:59 -0700

    feat: authors page styling

--- apps/web/src/features/author/components/author-posts.tsx
+++ apps/web/src/features/author/components/author-posts.tsx
@@ -5,6 +5,8 @@ import { graphql } from 'relay-runtime';
 import Link from 'next/link';
 
 import dayjs from 'dayjs';
+
+const CONTENT_PREVIEW_LENGTH = 10;
 interface AuthorPostsProps {
   posts: authorPosts$key;
 }
@@ -32,20 +34,28 @@ export const AuthorPosts: FC<AuthorPostsProps> = ({ posts }) => {
     <div className="w-full">
       <h3>Posts:</h3>
       <ul className="w-full">
-        {data.edges.map(({ node }) => (
-          <Link href={`/posts/${node.id}`}>
-            <li
-              key={node.id}
-              className="min-w-52 px-8 py-4 my-2 border-1 border-black"
-            >
-              <h3 className="text-2xl">{node.title}</h3>
-              <div className="opacity-50 flex justify-between">
-                <p>{node.content}</p>
-                <p>{dayjs(node.createdAt).format('MMMM D, YYYY')}</p>
-              </div>
-            </li>
-          </Link>
-        ))}
+        {data.edges.map(({ node }) => {
+          const shortContent = node.content.slice(0, CONTENT_PREVIEW_LENGTH);
+          const hasMoreContent = node.content.length > CONTENT_PREVIEW_LENGTH;
+
+          return (
+            <Link href={`/posts/${node.id}`}>
+              <li
+                key={node.id}
+                className="min-w-52 px-8 py-2 my-8 border-black border-b"
+              >
+                <h3 className="text-2xl">{node.title}</h3>
+                <div className="opacity-50 flex justify-between">
+                  <p>
+                    {shortContent}
+                    {hasMoreContent && '...'}
+                  </p>
+                  <p>{dayjs(node.createdAt).format('MMMM D, YYYY')}</p>
+                </div>
+              </li>
+            </Link>
+          );
+        })}
       </ul>
     </div>
   );
