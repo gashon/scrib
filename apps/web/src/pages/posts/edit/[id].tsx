0732f95298c336574397a175c26dc848cc113102
GashonHussein <gashon96@gmail.com>
2023-05-16 17:04:25 -0700

    refactor: update draft controls

--- apps/web/src/pages/posts/edit/[id].tsx
+++ apps/web/src/pages/posts/edit/[id].tsx
@@ -58,11 +58,13 @@ export default function Post({ post: loadedPost }: Props) {
   return (
     <div className="w-screen min-h-screen flex p-36 justify-center">
       <div className="w-3/4 h-auto flex flex-col" style={{ height: '100%' }}>
-        {post.status !== 'published' && (
-          <div className="absolute top-5 right-5">
+        <div className="absolute top-5 right-5">
+          {post.status !== 'published' ? (
             <Button onClick={() => handleSave('published')}>Publish</Button>
-          </div>
-        )}
+          ) : (
+            <Button onClick={() => handleSave('draft')}>Revert to Draft</Button>
+          )}
+        </div>
 
         <main
           style={{
