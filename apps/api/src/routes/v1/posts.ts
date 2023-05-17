582221d5daeec41f00a20ecac8c95d83c474e346
GashonHussein <gashon96@gmail.com>
2023-05-16 22:01:43 -0700

    refactor: import http-status

--- apps/api/src/routes/v1/posts.ts
+++ apps/api/src/routes/v1/posts.ts
@@ -1,18 +1,16 @@
 import express from 'express';
 import Post from '@scrib/db/models/post';
+import status from 'http-status';
+
 const router: express.Router = express.Router();
 
 router.get('/:id', async (req: express.Request, res: express.Response) => {
   const post = await Post.findById(req.params.id);
 
-  if (!post) {
-    return res.status(404).json({ error: 'Post not found' });
+  if (!post || post.deleted_at) {
+    return res.status(status.NOT_FOUND).json({ message: 'Post not found' });
   }
 
-  if(post.deleted_at){
-    return res.status(404).json({ error: 'Post not found' });
-  }
-  
   return res.json({ data: post });
 });
 
